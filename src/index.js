cb36a0a5199cd350fb2f4f3d3fc04068e241564d
Nataleesha <frojlan@gmail.com>
2023-02-11 05:37:04 +0200

    search

--- src/index.js
+++ src/index.js
@@ -7,25 +7,26 @@ const searchForm = document.querySelector('.search-form');
 const input = document.querySelector('[name="searchQuery"]');
 const gallery = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
+let searchValue = '';
+let images = [];
+let totalImages = 0;
 let page = 1;
 
 searchForm.addEventListener('submit', onSearch);
 
 async function onSearch(event) {
   event.preventDefault();
-
   loadBtn.hidden = true;
-  page = 1;
   gallery.innerHTML = '';
-  let searchValue = input.value.trim();
+  searchValue = input.value.trim();
 
   if (searchValue === '') {
     return;
   }
 
   const response = await searchImages(searchValue, page);
-  let totalImages = response.data.totalHits;
-  const images = response.data.hits;
+  totalImages = response.data.totalHits;
+  images = response.data.hits;
 
   if (images.length === 0) {
     Notify.failure(
@@ -33,20 +34,32 @@ async function onSearch(event) {
     );
     input.value = '';
   } else {
-    renderImages(images);
+    Notify.success(`Hooray! We found ${totalImages} images`);
 
-    totalImages -= 40;
+    renderImages(images);
 
     if (totalImages >= 40) {
       loadBtn.hidden = false;
-      loadBtn.addEventListener('click', () => {
-        page += 1;
-        searchImages(searchValue, page);
-        renderImages(images);
-      });
+      loadBtn.addEventListener('click', onLoadMore);
     } else {
+      loadBtn.removeEventListener('click', onLoadMore);
       Notify.info("We're sorry, but you've reached the end of search results.");
+      return;
     }
+  }
+}
+
+async function onLoadMore() {
+  totalImages -= 40;
+  page += 1;
+  const response = await searchImages(searchValue, page);
+  images = response.data.hits;
+  renderImages(images);
+  if (totalImages <= 40) {
+    loadBtn.hidden = true;
+    loadBtn.removeEventListener('click', onLoadMore);
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    return;
   }
 }
 
